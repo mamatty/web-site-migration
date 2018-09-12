fb051575dcd140b40cb2b5dd1ad8438a0a9ce97b
mamatty <matteo.novembre94@gmail.com>
2018-09-12 19:17:43 +0200

    Add files via upload

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -3,81 +3,81 @@
 session_start();
 
 // Check if user is logged in using the session variable
-if ( isset($_SESSION['logged_in']) and $_SESSION['logged_in'] == true) {
+if ( isset($_COOKIE['logged_in']) and $_COOKIE['logged_in'] == true) {
 // Makes it easier to read
-$first_name = $_SESSION['first_name'];
-$last_name = $_SESSION['last_name'];
+$first_name = $_COOKIE['first_name'];
+$last_name = $_COOKIE['last_name'];
+
 ?>
     <!DOCTYPE html>
     <html lang="en">
     <head>
-    <title>Fitness Club</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/reset.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/grid_12.css">
+        <title>Fitness Club</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/grid_12.css">
 
-    <script src="js/jquery-1.7.min.js"></script>
-    <script src="js/jquery.easing.1.3.js"></script>
-    <script src="js/tms-0.3.js"></script>
-    <script src="js/tms_presets.js"></script>
-    <script src="js/cufon-yui.js"></script>
-    <script src="js/Asap_400.font.js"></script>
-    <script src="js/Coolvetica_400.font.js"></script>
-    <script src="js/Kozuka_M_500.font.js"></script>
-    <script src="js/cufon-replace.js"></script>
-    <script src="js/FF-cash.js"></script>
+        <script src="js/jquery-1.7.min.js"></script>
+        <script src="js/jquery.easing.1.3.js"></script>
+        <script src="js/tms-0.3.js"></script>
+        <script src="js/tms_presets.js"></script>
+        <script src="js/cufon-yui.js"></script>
+        <script src="js/Asap_400.font.js"></script>
+        <script src="js/Coolvetica_400.font.js"></script>
+        <script src="js/Kozuka_M_500.font.js"></script>
+        <script src="js/cufon-replace.js"></script>
+        <script src="js/FF-cash.js"></script>
 
-    <!--[if lt IE 9]>
-    <script src="js/html5.js"></script>
-    <link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
-    <![endif]-->
+        <!--[if lt IE 9]>
+        <script src="js/html5.js"></script>
+        <link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+        <![endif]-->
 
-    <!-- Latest compiled and minified Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+        <!-- Latest compiled and minified Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 
-    <!-- custom css -->
-    <style>
-        .m-r-1em{ margin-right:1em; }
-        .m-b-1em{ margin-bottom:1em; }
-        .m-l-1em{ margin-left:1em; }
-        .mt0{ margin-top:0; }
-    </style>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        #searchList ul{
-            background-color: #c2c2c2;
-            cursor:pointer;
-        }
-        li{
-            padding:2px;
-        }
-    </style>
-    <style>
-        input[type=text] {
-            width: 220px;
-            box-sizing: border-box;
-            border: 2px solid #ccc;
-            border-radius: 4px;
-            font-size: 16px;
-            background-color: white;
-            background-position: 10px 10px;
-            background-repeat: no-repeat;
-            padding: 12px 20px 12px 40px;
-            -webkit-transition: width 0.4s ease-in-out;
-            transition: width 0.4s ease-in-out;
-        }
+        <!-- custom css -->
+        <style>
+            .m-r-1em{ margin-right:1em; }
+            .m-b-1em{ margin-bottom:1em; }
+            .m-l-1em{ margin-left:1em; }
+            .mt0{ margin-top:0; }
+        </style>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+            #searchList ul{
+                background-color: #c2c2c2;
+                cursor:pointer;
+            }
+            li{
+                padding:2px;
+            }
+        </style>
+        <style>
+            input[type=text] {
+                width: 220px;
+                box-sizing: border-box;
+                border: 2px solid #ccc;
+                border-radius: 4px;
+                font-size: 16px;
+                background-color: white;
+                background-position: 10px 10px;
+                background-repeat: no-repeat;
+                padding: 12px 20px 12px 40px;
+                -webkit-transition: width 0.4s ease-in-out;
+                transition: width 0.4s ease-in-out;
+            }
 
-        input[type=text]:focus {
-            width: 100%;
-        }
-    </style>
-</head>
-<body>
-
+            input[type=text]:focus {
+                width: 100%;
+            }
+        </style>
+    </head>
+    <body>
 <!--==============================header=================================-->
 <!-- container -->
 <div class="main">
@@ -96,8 +96,8 @@ $last_name = $_SESSION['last_name'];
         </nav>
     </header>
     <?php
-    require_once "DbOperation.php";
-    $conn = new DbOperation();
+    require_once "../DbOperations/DbOperationDashboard.php";
+    $conn = new DbOperationDashboard();
     $req = $conn->get_data();
     $data = json_decode($req,True);
     list($temperature, $humidity) = $conn->tmp_hum($data);
@@ -108,8 +108,8 @@ $last_name = $_SESSION['last_name'];
 
 
     <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/565919/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line"></iframe>
-    <h3 style="color:red;">Today's Temperature(mean): <?php echo $temperature?></h3>
-    <h3 style="color:#461cff;">Today's Humidity(mean): <?php echo $humidity?></h3>
+    <h3 style="color:#0cff80;">Today's Temperature(avg): <?php echo $temperature?> °C</h3>
+    <h3 style="color:#461cff;">Today's Humidity(avg): <?php echo $humidity?> g/m³</h3>
     <footer>
         <p>© 2018 Fitness Club</p>
         <p>Design for Smart Gym</p>
@@ -122,9 +122,7 @@ $last_name = $_SESSION['last_name'];
     <?php
 }
 else {
-
-    ?><!-- Latest compiled and minified Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    ?><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
     <style>
         .button {
             background-color: #518daf;
@@ -145,6 +143,7 @@ else {
     <?php
     echo "<div align='center' class='alert alert-danger'>You must be logged to view this page.</div>";
     echo "<a href=\"../login_website/index.php\"><button class=\"button\"><span>Log In</span>
-                        </button></a>";
+</button></a>";
+
 }
 ?>
