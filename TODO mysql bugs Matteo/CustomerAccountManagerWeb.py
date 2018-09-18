0ec733526a533453dab9f3f14dac79d389a792e5
mamatty <matteo.novembre94@gmail.com>
2018-09-18 22:48:10 +0200

    Update CustomerAccountManagerWeb.py

--- TODO mysql bugs Matteo/CustomerAccountManagerWeb.py
+++ TODO mysql bugs Matteo/CustomerAccountManagerWeb.py
@@ -671,7 +671,7 @@ class CustomAccountManagerMobileWeb(CustomerAccountManagerBase):
 
             # retrieve parameters
             email = p.get("email", None)
-            password = p.get('password')
+            password = p.get('password', None)
 
             check_mandatory_parameters([email, password], ["email", "password"])
 
